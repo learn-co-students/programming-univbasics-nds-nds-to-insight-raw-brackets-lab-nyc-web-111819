3c34dc679c052b0074c13613022c6b71ea92163b
Madeline Corman <madelinecorman@gmail.com>
2019-11-09 20:13:39 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,9 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
+#require 'pp'
+#pp directors_database
+
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
@@ -18,5 +21,18 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  results = {}
+  row_index = 0 
+    while row_index < directors_database.length do 
+      name = directors_database[row_index][:name]
+      gross_totals = []
+      director_hash = directors_database[row_index]
+      movies_array = director_hash[:movies]
+      movies_array.each do |movie|
+        gross_totals << movie[:worldwide_gross]
+      end
+      results.merge!(name => gross_totals.sum)
+      row_index += 1
+    end
+  return results
 end
